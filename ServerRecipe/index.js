8cdf3856828d479995eb47d6dff33cfa2801cca7
khushi Vashisth <khushidhiman951@gmail.com>
2024-08-01 19:39:17 +0530

    LOGIN SYSTEM

---
+++ ServerRecipe/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { UserRouter } from "../ServerRecipe/src/routes/users.js";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+const port = 8000;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`running on port: ${port}`);
+    });
+  } catch (err) {
+    console.error("Could not connect to MongoDB...", err);
+    process.exit(0);
+  }
+};
+connectDB();
+
+app.use("/auth", UserRouter);
+
+app.get("/", (req, res) => {
+  res.send("welcome kd");
+});
---
+++ ServerRecipe/src/models/users.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const UserModel = mongoose.model("users", UserSchema);
